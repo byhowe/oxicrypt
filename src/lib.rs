d9a0fd345f18849f470926d45d60642c95a9b84b
B. Howe <37745048+byhowe@users.noreply.github.com>
2021-05-25 22:34:44 +0200

    change the way len is appended to the block in sha

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,9 @@
+//! # The OxiCrypt Rust Library
+//!
+//! OxiCrypt is a cryptography library written mainly in Rust.
 #![feature(new_uninit)]
 #![feature(maybe_uninit_ref)]
+#![feature(num_as_ne_bytes)]
 
 pub mod sha;
 
