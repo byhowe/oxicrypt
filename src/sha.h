25fc32fd0ea182180286251c29ab7deedb3ea193
B. Howe <37745048+byhowe@users.noreply.github.com>
2021-05-26 11:14:22 +0200

    minor formatting

--- src/sha.h
+++ src/sha.h
@@ -12,9 +12,7 @@ extern "C" {
 /* Compress functions from Rust. */
 
 void oxi_sha1_compress_generic(uint32_t* state, const uint8_t* block);
-
 void oxi_sha256_compress_generic(uint32_t* state, const uint8_t* block);
-
 void oxi_sha512_compress_generic(uint64_t* state, const uint8_t* block);
 
 /* Type definitions. */
