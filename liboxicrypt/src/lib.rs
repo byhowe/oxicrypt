f9c90fd7ce14543f9798bdf266420a447b3a5260
B. Howe <37745048+byhowe@users.noreply.github.com>
2021-05-28 20:40:10 +0200

    change liboxicrypt lib name

--- liboxicrypt/src/lib.rs
+++ liboxicrypt/src/lib.rs
@@ -9,21 +9,21 @@ fn panic(_panic: &PanicInfo<'_>) -> !
   loop {}
 }
 
-/// See [`oxicrypt::sha::sha1_compress_generic`].
+/// See [`oxicrypt_core::sha::sha1_compress_generic`].
 #[no_mangle]
 pub unsafe extern "C" fn oxi_sha1_compress_generic(state: *mut u32, block: *const u8)
 {
   oxicrypt_core::sha::sha1_compress_generic(state, block);
 }
 
-/// See [`oxicrypt::sha::sha256_compress_generic`].
+/// See [`oxicrypt_core::sha::sha256_compress_generic`].
 #[no_mangle]
 pub unsafe extern "C" fn oxi_sha256_compress_generic(state: *mut u32, block: *const u8)
 {
   oxicrypt_core::sha::sha256_compress_generic(state, block);
 }
 
-/// See [`oxicrypt::sha::sha256_compress_generic`].
+/// See [`oxicrypt_core::sha::sha256_compress_generic`].
 #[no_mangle]
 pub unsafe extern "C" fn oxi_sha512_compress_generic(state: *mut u64, block: *const u8)
 {
