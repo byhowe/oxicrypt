1703aad7ff48742248011ff9628359e10b93025b
B. Howe <37745048+byhowe@users.noreply.github.com>
2021-06-07 22:49:58 +0200

    Remove the asm feature from the test script.

--- test_multi_arch.sh
+++ test_multi_arch.sh
@@ -3,28 +3,14 @@
 # Run tests for platforms other than the current one.
 # Only tested on linux.
 
-export RUSTFLAGS="-C target-cpu=native"
+cargo test --package oxicrypt-core --target x86_64-unknown-linux-gnu -- --ignored
+cargo test --package oxicrypt-core --target x86_64-unknown-linux-gnu --release -- --ignored
 
-cargo test --package oxicrypt-core --target x86_64-unknown-linux-gnu
-cargo test --package oxicrypt-core --target x86_64-unknown-linux-gnu --release
+cargo test --package oxicrypt-core --target i686-unknown-linux-gnu -- --ignored
+cargo test --package oxicrypt-core --target i686-unknown-linux-gnu --release -- --ignored
 
-cargo test --package oxicrypt-core --target x86_64-unknown-linux-gnu --features asm
-cargo test --package oxicrypt-core --target x86_64-unknown-linux-gnu --release --features asm
+cargo test --package oxicrypt-core --target x86_64-pc-windows-gnu -- --ignored
+cargo test --package oxicrypt-core --target x86_64-pc-windows-gnu --release -- --ignored
 
-cargo test --package oxicrypt-core --target i686-unknown-linux-gnu
-cargo test --package oxicrypt-core --target i686-unknown-linux-gnu --release
-
-cargo test --package oxicrypt-core --target i686-unknown-linux-gnu --features asm
-cargo test --package oxicrypt-core --target i686-unknown-linux-gnu --release --features asm
-
-cargo test --package oxicrypt-core --target x86_64-pc-windows-gnu
-cargo test --package oxicrypt-core --target x86_64-pc-windows-gnu --release
-
-cargo test --package oxicrypt-core --target x86_64-pc-windows-gnu --features asm
-cargo test --package oxicrypt-core --target x86_64-pc-windows-gnu --release --features asm
-
-cargo test --package oxicrypt-core --target i686-pc-windows-gnu
-cargo test --package oxicrypt-core --target i686-pc-windows-gnu --release
-
-cargo test --package oxicrypt-core --target i686-pc-windows-gnu --features asm
-cargo test --package oxicrypt-core --target i686-pc-windows-gnu --release --features asm
+cargo test --package oxicrypt-core --target i686-pc-windows-gnu -- --ignored
+cargo test --package oxicrypt-core --target i686-pc-windows-gnu --release -- --ignored
