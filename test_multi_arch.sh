bcee9618bf2d74a7c19f733dbb778a6afc5e4a66
B. Howe <37745048+byhowe@users.noreply.github.com>
2021-06-14 02:25:27 +0200

    Remove --ignored flags.

--- test_multi_arch.sh
+++ test_multi_arch.sh
@@ -3,14 +3,14 @@
 # Run tests for platforms other than the current one.
 # Only tested on linux.
 
-cargo test --package oxicrypt-core --target x86_64-unknown-linux-gnu -- --ignored
-cargo test --package oxicrypt-core --target x86_64-unknown-linux-gnu --release -- --ignored
+cargo test --package oxicrypt-core --target x86_64-unknown-linux-gnu
+cargo test --package oxicrypt-core --target x86_64-unknown-linux-gnu --release
 
-cargo test --package oxicrypt-core --target i686-unknown-linux-gnu -- --ignored
-cargo test --package oxicrypt-core --target i686-unknown-linux-gnu --release -- --ignored
+cargo test --package oxicrypt-core --target i686-unknown-linux-gnu
+cargo test --package oxicrypt-core --target i686-unknown-linux-gnu --release
 
-cargo test --package oxicrypt-core --target x86_64-pc-windows-gnu -- --ignored
-cargo test --package oxicrypt-core --target x86_64-pc-windows-gnu --release -- --ignored
+cargo test --package oxicrypt-core --target x86_64-pc-windows-gnu
+cargo test --package oxicrypt-core --target x86_64-pc-windows-gnu --release
 
-cargo test --package oxicrypt-core --target i686-pc-windows-gnu -- --ignored
-cargo test --package oxicrypt-core --target i686-pc-windows-gnu --release -- --ignored
+cargo test --package oxicrypt-core --target i686-pc-windows-gnu
+cargo test --package oxicrypt-core --target i686-pc-windows-gnu --release
