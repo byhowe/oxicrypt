62ab93136f7826d5d68b53ea18de421a0ad394cf
B. Howe <37745048+byhowe@users.noreply.github.com>
2021-06-20 19:53:59 +0200

    Add documentation to oxicrypt/sha.

--- oxicrypt/src/sha.rs
+++ oxicrypt/src/sha.rs
@@ -1,10 +1,12 @@
+//! SHA family of hash functions.
+
 use std::cmp;
 use std::mem;
 use std::mem::MaybeUninit;
 
-use oxicrypt_core::sha::sha1_compress_generic;
-use oxicrypt_core::sha::sha256_compress_generic;
-use oxicrypt_core::sha::sha512_compress_generic;
+use oxicrypt_core::sha::generic::sha1_compress_generic;
+use oxicrypt_core::sha::generic::sha256_compress_generic;
+use oxicrypt_core::sha::generic::sha512_compress_generic;
 use oxicrypt_core::sha::H1;
 use oxicrypt_core::sha::H224;
 use oxicrypt_core::sha::H256;
@@ -15,6 +17,7 @@ use oxicrypt_core::sha::H512_256;
 
 macro_rules! impl_sha {
   (
+    $algo_str:expr;
     struct $algo:ident;
     type Len = $lenty:ty;
     const INITIAL_H: [$uint:ty; $statelen:expr] = $H:ident;
@@ -22,6 +25,7 @@ macro_rules! impl_sha {
     const DIGEST_LEN: usize = $digestlen:expr;
     fn compress = $compressfn:ident;
   ) => {
+    #[doc = concat!($algo_str, " algorithm.")]
     pub struct $algo
     {
       h: [$uint; $statelen],
@@ -32,6 +36,16 @@ macro_rules! impl_sha {
 
     impl $algo
     {
+      /// Creates a new instance of
+      #[doc = $algo_str]
+      /// on the stack.
+      ///
+      /// # Examples
+      ///
+      /// ```
+      #[doc = concat!("use oxicrypt::sha::", stringify!($algo), ";")]
+      #[doc = concat!("let mut ctx = ", stringify!($algo), "::new();")]
+      /// ```
       pub const fn new() -> Self
       {
         Self {
@@ -42,6 +56,16 @@ macro_rules! impl_sha {
         }
       }
 
+      /// Creates a new instance of
+      #[doc = $algo_str]
+      /// on the heap.
+      ///
+      /// # Examples
+      ///
+      /// ```
+      #[doc = concat!("use oxicrypt::sha::", stringify!($algo), ";")]
+      #[doc = concat!("let mut ctx = ", stringify!($algo), "::new_boxed();")]
+      /// ```
       pub fn new_boxed() -> Box<Self>
       {
         let mut ctx: Box<MaybeUninit<Self>> = Box::new_uninit();
@@ -49,6 +73,23 @@ macro_rules! impl_sha {
         unsafe { ctx.assume_init() }
       }
 
+      /// Resets the context to its original state.
+      ///
+      /// # Examples
+      ///
+      /// ```
+      #[doc = concat!("use oxicrypt::sha::", stringify!($algo), ";")]
+      #[doc = concat!("let mut ctx = ", stringify!($algo), "::new();")]
+      /// ctx.update(b"Hello World");
+      /// // Digest value of 'Hello World'.
+      /// let hello_world_digest = ctx.finish();
+      ///
+      /// ctx.reset();
+      ///
+      /// ctx.update(b"Hei Verden");
+      /// // Digest value of 'Hei Verden'.
+      /// let hei_verden_digest = ctx.finish();
+      /// ```
       pub fn reset(&mut self)
       {
         self.h = $H;
@@ -57,6 +98,15 @@ macro_rules! impl_sha {
         self.blocklen = 0;
       }
 
+      /// Updates state with the given data.
+      ///
+      /// # Examples
+      ///
+      /// ```
+      #[doc = concat!("use oxicrypt::sha::", stringify!($algo), ";")]
+      #[doc = concat!("let mut ctx = ", stringify!($algo), "::new();")]
+      /// ctx.update(b"Hello World");
+      /// ```
       pub fn update(&mut self, mut data: &[u8])
       {
         while !data.is_empty() {
@@ -79,6 +129,16 @@ macro_rules! impl_sha {
         }
       }
 
+      /// Returns the digest of the data the context has been fed using [`update`](`Self::update`).
+      ///
+      /// # Examples
+      ///
+      /// ```
+      #[doc = concat!("use oxicrypt::sha::", stringify!($algo), ";")]
+      #[doc = concat!("let mut ctx = ", stringify!($algo), "::new();")]
+      /// ctx.update(b"Hello World");
+      /// let digest = ctx.finish();
+      /// ```
       pub fn finish(&mut self) -> [u8; $digestlen]
       {
         let mut output = [0; $digestlen];
@@ -86,6 +146,17 @@ macro_rules! impl_sha {
         output
       }
 
+      /// Returns the digest of the data the context has been fed using [`update`](`Self::update`).
+      /// The data returned lives on the heap.
+      ///
+      /// # Examples
+      ///
+      /// ```
+      #[doc = concat!("use oxicrypt::sha::", stringify!($algo), ";")]
+      #[doc = concat!("let mut ctx = ", stringify!($algo), "::new();")]
+      /// ctx.update(b"Hello World");
+      /// let digest = ctx.finish_boxed();
+      /// ```
       pub fn finish_boxed(&mut self) -> Box<[u8]>
       {
         let mut output = unsafe { Box::new_uninit_slice($digestlen).assume_init() };
@@ -93,6 +164,21 @@ macro_rules! impl_sha {
         output
       }
 
+      /// Writes the digest of the data the context has been fed using [`update`](`Self::update`)
+      /// into `output`. Note that if the length of `output` is less than
+      #[doc = stringify!($digestlen)]
+      /// , digest gets truncated.
+      ///
+      /// # Examples
+      ///
+      /// ```
+      #[doc = concat!("use oxicrypt::sha::", stringify!($algo), ";")]
+      #[doc = concat!("let mut ctx = ", stringify!($algo), "::new();")]
+      /// ctx.update(b"Hello World");
+      /// // Only the first 15 bytes of the digest will get written.
+      /// let mut digest: Vec<u8> = vec![0; 15];
+      /// let digest = ctx.finish_into(&mut digest);
+      /// ```
       pub fn finish_into(&mut self, output: &mut [u8])
       {
         self.block[self.blocklen] = 0b10000000;
@@ -123,6 +209,14 @@ macro_rules! impl_sha {
         self.reset();
       }
 
+      /// Returns the digest of `data` in one go.
+      ///
+      /// # Examples
+      ///
+      /// ```
+      #[doc = concat!("use oxicrypt::sha::", stringify!($algo), ";")]
+      #[doc = concat!("let digest = ", stringify!($algo), "::oneshot(b\"Hello World\");")]
+      /// ```
       pub fn oneshot(data: &[u8]) -> [u8; $digestlen]
       {
         let mut ctx = Self::new();
@@ -130,6 +224,14 @@ macro_rules! impl_sha {
         ctx.finish()
       }
 
+      /// Returns the digest of `data` in one go. The data returned lives on the heap.
+      ///
+      /// # Examples
+      ///
+      /// ```
+      #[doc = concat!("use oxicrypt::sha::", stringify!($algo), ";")]
+      #[doc = concat!("let digest = ", stringify!($algo), "::oneshot_boxed(b\"Hello World\");")]
+      /// ```
       pub fn oneshot_boxed(data: &[u8]) -> Box<[u8]>
       {
         let mut ctx = Self::new();
@@ -137,6 +239,18 @@ macro_rules! impl_sha {
         ctx.finish_boxed()
       }
 
+      /// Returns the digest of `data` in one go. Note that if the length of `output` is less than
+      #[doc = stringify!($digestlen)]
+      /// , digest gets truncated.
+      ///
+      /// # Examples
+      ///
+      /// ```
+      #[doc = concat!("use oxicrypt::sha::", stringify!($algo), ";")]
+      /// // Only the first 15 bytes of the digest will get written.
+      /// let mut digest: Vec<u8> = vec![0; 15];
+      #[doc = concat!("let digest = ", stringify!($algo), "::oneshot_into(b\"Hello World\", &mut digest);")]
+      /// ```
       pub fn oneshot_into(data: &[u8], output: &mut [u8])
       {
         let mut ctx = Self::new();
@@ -148,6 +262,7 @@ macro_rules! impl_sha {
 }
 
 impl_sha! {
+    "SHA-1";
     struct Sha1;
     type Len = u64;
     const INITIAL_H: [u32; 5] = H1;
@@ -157,6 +272,7 @@ impl_sha! {
 }
 
 impl_sha! {
+    "SHA-224";
     struct Sha224;
     type Len = u64;
     const INITIAL_H: [u32; 8] = H224;
@@ -166,6 +282,7 @@ impl_sha! {
 }
 
 impl_sha! {
+    "SHA-256";
     struct Sha256;
     type Len = u64;
     const INITIAL_H: [u32; 8] = H256;
@@ -175,6 +292,7 @@ impl_sha! {
 }
 
 impl_sha! {
+    "SHA-384";
     struct Sha384;
     type Len = u128;
     const INITIAL_H: [u64; 8] = H384;
@@ -184,6 +302,7 @@ impl_sha! {
 }
 
 impl_sha! {
+    "SHA-512";
     struct Sha512;
     type Len = u128;
     const INITIAL_H: [u64; 8] = H512;
@@ -193,6 +312,7 @@ impl_sha! {
 }
 
 impl_sha! {
+    "SHA-512/224";
     struct Sha512_224;
     type Len = u128;
     const INITIAL_H: [u64; 8] = H512_224;
@@ -202,6 +322,7 @@ impl_sha! {
 }
 
 impl_sha! {
+    "SHA-512/256";
     struct Sha512_256;
     type Len = u128;
     const INITIAL_H: [u64; 8] = H512_256;
