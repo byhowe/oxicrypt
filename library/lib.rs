4da2d63faa19d6b4a8338ea1f2d9ba7c3c2465d8
B. Howe <37745048+byhowe@users.noreply.github.com>
2021-08-04 15:24:58 +0200

    Remove stabilized feature usage.

--- library/lib.rs
+++ library/lib.rs
@@ -6,7 +6,6 @@
 #![feature(const_unreachable_unchecked)]
 #![feature(const_mut_refs)]
 #![feature(const_ptr_offset)]
-#![feature(const_fn_transmute)]
 #![feature(const_maybe_uninit_assume_init)]
 #![feature(const_raw_ptr_deref)]
 #![feature(const_fn_trait_bound)]
