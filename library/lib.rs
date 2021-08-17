00d6e8ee5926d53e0e6d35c04cf2418ec58f51c0
B. Howe <37745048+byhowe@users.noreply.github.com>
2021-08-17 23:50:19 +0200

    Add `Control` object to enable configuration of the library at runtime.

--- library/lib.rs
+++ library/lib.rs
@@ -29,3 +29,37 @@ pub mod sha;
 
 #[cfg(test)]
 pub(crate) mod test_vectors;
+
+static mut GLOBAL_IMPL: Implementation = Implementation::new();
+
+/// Tweak certain aspects of the library.
+pub enum Control {}
+
+impl Control
+{
+  /// Set the global implementation details.
+  ///
+  /// The globa implementation variable is used when using functions that do not accept an
+  /// implementation variable or when using functions from traits (e.g. `std::io::Write`).
+  ///
+  /// # Safety
+  ///
+  /// Global implementation is stored in a mutable static variable that is **not** behind a mutex
+  /// guard. Although it should not be a problem, it is still better to call this function before
+  /// using any more of the library's functions (e.g. in `fn main() {}`).
+  pub fn set_global_implementation(implementation: Implementation)
+  {
+    unsafe { GLOBAL_IMPL = implementation }
+  }
+
+  /// Get the global implementation variable.
+  pub fn get_global_implementation() -> Implementation
+  {
+    unsafe { GLOBAL_IMPL }
+  }
+
+  /// Get a mutable reference to the global implementation variable.
+  pub fn get_global_implementation_mut() -> &'static mut Implementation {
+    unsafe { &mut GLOBAL_IMPL }
+  }
+}
