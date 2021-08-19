0850e033d5ff676028fcee85c82e0ec54278d1aa
B. Howe <37745048+byhowe@users.noreply.github.com>
2021-08-19 16:46:39 +0200

    Rewrite HKDF.

--- library/hkdf.rs
+++ library/hkdf.rs
@@ -12,26 +12,27 @@
 //!
 //! ```
 //! use oxicrypt::hmac::HmacSha256;
-//! use oxicrypt::sha::Implementation;
 //!
-//! let i = Implementation::fastest_rt();
 //! let salt = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12];
 //! let ikm = [11; 22];
-//! let prk = HmacSha256::oneshot(i, salt, ikm);
+//! let prk = HmacSha256::oneshot(&salt, &ikm);
 //! ```
 
+use core::fmt::Debug;
+use core::fmt::Display;
 use core::mem::MaybeUninit;
 
 use crate::hmac::Hmac;
 use crate::sha;
+use crate::Control;
 use crate::Implementation;
 
 /// HKDF-Expand.
 #[derive(Debug, Clone, Copy)]
 #[cfg_attr(feature = "c", repr(C))]
-pub struct Hkdf<D, const M: usize, const O: usize, const B: usize>
+pub struct Hkdf<D, const M: u64, const O: usize, const B: usize>
 where
-  D: core::fmt::Debug + Clone + Copy,
+  D: Debug + Clone + Copy,
 {
   hmac: Hmac<D, M, O, B>,
 }
@@ -51,13 +52,17 @@ pub type HkdfSha512_224 = Hkdf<sha::Sha512_224, 0x5ba_512_224, 28, 128>;
 /// HKDF-SHA-512/256
 pub type HkdfSha512_256 = Hkdf<sha::Sha512_256, 0x5ba_512_256, 32, 128>;
 
-impl<D, const M: usize, const O: usize, const B: usize> Hkdf<D, M, O, B>
+impl<D, const M: u64, const O: usize, const B: usize> Hkdf<D, M, O, B>
 where
-  D: core::fmt::Debug + Clone + Copy,
+  D: Debug + Clone + Copy,
 {
-  pub fn with_prk<K: AsRef<[u8]>>(implementation: Implementation, prk: K) -> Result<Self, LenError>
+  pub fn with_prk(prk: &[u8]) -> Result<Self, LenError>
   {
-    let prk = prk.as_ref();
+    Self::with_prk_impl(Control::get_global_implementation(), prk)
+  }
+
+  pub fn with_prk_impl(implementation: Implementation, prk: &[u8]) -> Result<Self, LenError>
+  {
     if prk.len() < O {
       return Err(LenError::Prk {
         at_least: O,
@@ -65,36 +70,55 @@ where
       });
     }
     let mut ctx: MaybeUninit<Self> = MaybeUninit::uninit();
-    unsafe { ctx.assume_init_mut().set_prk_unchecked(implementation, prk) };
+    unsafe { ctx.assume_init_mut().set_prk_unchecked_impl(implementation, prk) };
     Ok(unsafe { ctx.assume_init() })
   }
 
-  pub unsafe fn with_prk_unchecked<K: AsRef<[u8]>>(implementation: Implementation, prk: K) -> Self
+  pub unsafe fn with_prk_unchecked(prk: &[u8]) -> Self
+  {
+    Self::with_prk_unchecked_impl(Control::get_global_implementation(), prk)
+  }
+
+  pub unsafe fn with_prk_unchecked_impl(implementation: Implementation, prk: &[u8]) -> Self
   {
     let mut ctx: MaybeUninit<Self> = MaybeUninit::uninit();
-    ctx.assume_init_mut().set_prk_unchecked(implementation, prk);
+    ctx.assume_init_mut().set_prk_unchecked_impl(implementation, prk);
     ctx.assume_init()
   }
 
-  pub fn set_prk<K: AsRef<[u8]>>(&mut self, implementation: Implementation, prk: K) -> Result<(), LenError>
+  pub fn set_prk(&mut self, prk: &[u8]) -> Result<(), LenError>
   {
-    let prk = prk.as_ref();
+    self.set_prk_impl(Control::get_global_implementation(), prk)
+  }
+
+  pub fn set_prk_impl(&mut self, implementation: Implementation, prk: &[u8]) -> Result<(), LenError>
+  {
     if prk.len() < O {
       return Err(LenError::Prk {
         at_least: O,
         got: prk.len(),
       });
     }
-    unsafe { self.set_prk_unchecked(implementation, prk) };
+    unsafe { self.set_prk_unchecked_impl(implementation, prk) };
     Ok(())
   }
 
-  pub unsafe fn set_prk_unchecked<K: AsRef<[u8]>>(&mut self, implementation: Implementation, prk: K)
+  pub unsafe fn set_prk_unchecked(&mut self, prk: &[u8])
   {
-    self.hmac.set_key(implementation, prk);
+    self.set_prk_unchecked_impl(Control::get_global_implementation(), prk);
   }
 
-  pub fn expand(&self, implementation: Implementation, info: &[&[u8]], okm: &mut [u8]) -> Result<(), LenError>
+  pub unsafe fn set_prk_unchecked_impl(&mut self, implementation: Implementation, prk: &[u8])
+  {
+    self.hmac.set_key_impl(implementation, prk);
+  }
+
+  pub fn expand(&self, info: &[&[u8]], okm: &mut [u8]) -> Result<(), LenError>
+  {
+    self.expand_impl(Control::get_global_implementation(), info, okm)
+  }
+
+  pub fn expand_impl(&self, implementation: Implementation, info: &[&[u8]], okm: &mut [u8]) -> Result<(), LenError>
   {
     if okm.len() > 255 * O {
       return Err(LenError::Okm {
@@ -102,11 +126,16 @@ where
         got: okm.len(),
       });
     }
-    unsafe { self.expand_unchecked(implementation, info, okm) };
+    unsafe { self.expand_unchecked_impl(implementation, info, okm) };
     Ok(())
   }
 
-  pub unsafe fn expand_unchecked(&self, implementation: Implementation, info: &[&[u8]], okm: &mut [u8])
+  pub unsafe fn expand_unchecked(&self, info: &[&[u8]], okm: &mut [u8])
+  {
+    self.expand_unchecked_impl(Control::get_global_implementation(), info, okm);
+  }
+
+  pub unsafe fn expand_unchecked_impl(&self, implementation: Implementation, info: &[&[u8]], okm: &mut [u8])
   {
     let mut t: &[u8] = b"";
     let mut i = 0x00;
@@ -114,10 +143,10 @@ where
     for chunk in okm.chunks_mut(O) {
       i += 1;
       hmac = self.hmac;
-      hmac.update(implementation, t);
-      info.iter().for_each(|i| hmac.update(implementation, i));
-      hmac.update(implementation, &[i]);
-      hmac.finish_into(implementation, chunk);
+      hmac.update_impl(implementation, t);
+      info.iter().for_each(|i| hmac.update_impl(implementation, i));
+      hmac.update_impl(implementation, &[i]);
+      hmac.finish_into_impl(implementation, chunk);
       t = chunk;
     }
   }
@@ -148,7 +177,7 @@ impl LenError
   }
 }
 
-impl core::fmt::Display for LenError
+impl Display for LenError
 {
   fn fmt(&self, f: &mut core::fmt::Formatter<'_>) -> core::fmt::Result
   {
