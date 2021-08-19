e5bacc5c3be83152f59eb22d94390031d5618ebe
B. Howe <37745048+byhowe@users.noreply.github.com>
2021-08-19 03:56:12 +0200

    Rewrite SHA in the C lib.

--- oxicrypt_c/src/sha.rs
+++ oxicrypt_c/src/sha.rs
@@ -3,7 +3,6 @@
 use core::slice;
 
 use oxicrypt::hazmat::sha;
-use oxicrypt::hazmat::sha::Variant;
 use oxicrypt::sha::Sha1;
 use oxicrypt::sha::Sha224;
 use oxicrypt::sha::Sha256;
@@ -11,574 +10,202 @@ use oxicrypt::sha::Sha384;
 use oxicrypt::sha::Sha512;
 use oxicrypt::sha::Sha512_224;
 use oxicrypt::sha::Sha512_256;
-use oxicrypt::Implementation;
 
 use crate::oxi_implementation_t;
 
 // Raw SHA functions.
 
 #[no_mangle]
-pub unsafe extern "C" fn oxi_sha1_compress_generic(state: *mut u8, block: *const u8)
+pub unsafe extern "C" fn oxi_sha1_compress_generic(state: *mut u32, block: *const u8)
 {
   sha::generic::sha1_compress(state, block);
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn oxi_sha256_compress_generic(state: *mut u8, block: *const u8)
+pub unsafe extern "C" fn oxi_sha256_compress_generic(state: *mut u32, block: *const u8)
 {
   sha::generic::sha256_compress(state, block);
 }
 
 #[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_compress_generic(state: *mut u8, block: *const u8)
+pub unsafe extern "C" fn oxi_sha512_compress_generic(state: *mut u64, block: *const u8)
 {
   sha::generic::sha512_compress(state, block);
 }
 
-// Engine.
+// SHA engines.
 
 #[allow(non_camel_case_types)]
-#[repr(C)]
-pub struct oxi_sha_engine_t
-{
-  compress: unsafe extern "C" fn(*mut u8, *const u8),
-}
-
-impl oxi_sha_engine_t
-{
-  const E1_GENERIC: Self = unsafe { Self::new(Variant::Sha1, Implementation::new()) };
-  const E256_GENERIC: Self = unsafe { Self::new(Variant::Sha256, Implementation::new()) };
-  const E512_GENERIC: Self = unsafe { Self::new(Variant::Sha512, Implementation::new()) };
-
-  const unsafe fn new(variant: Variant, implementation: Implementation) -> Self
-  {
-    match implementation {
-      | _ => match variant {
-        | Variant::Sha1 => Self {
-          compress: oxi_sha1_compress_generic,
-        },
-        | Variant::Sha224 | Variant::Sha256 => Self {
-          compress: oxi_sha256_compress_generic,
-        },
-        | Variant::Sha384 | Variant::Sha512 | Variant::Sha512_224 | Variant::Sha512_256 => Self {
-          compress: oxi_sha512_compress_generic,
-        },
-      },
-    }
-  }
-
-  const unsafe fn as_ref(variant: Variant, implementation: Implementation) -> &'static Self
-  {
-    match implementation {
-      | _ => match variant {
-        | Variant::Sha1 => &Self::E1_GENERIC,
-        | Variant::Sha224 | Variant::Sha256 => &Self::E256_GENERIC,
-        | Variant::Sha384 | Variant::Sha512 | Variant::Sha512_224 | Variant::Sha512_256 => &Self::E512_GENERIC,
-      },
-    }
-  }
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha1_engine_new(implementation: oxi_implementation_t) -> oxi_sha_engine_t
-{
-  oxi_sha_engine_t::new(Variant::Sha1, implementation)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha1_engine_as_ref(implementation: oxi_implementation_t) -> *const oxi_sha_engine_t
-{
-  oxi_sha_engine_t::as_ref(Variant::Sha1, implementation)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha224_engine_new(implementation: oxi_implementation_t) -> oxi_sha_engine_t
-{
-  oxi_sha_engine_t::new(Variant::Sha224, implementation)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha224_engine_as_ref(implementation: oxi_implementation_t) -> *const oxi_sha_engine_t
-{
-  oxi_sha_engine_t::as_ref(Variant::Sha224, implementation)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha256_engine_new(implementation: oxi_implementation_t) -> oxi_sha_engine_t
-{
-  oxi_sha_engine_t::new(Variant::Sha256, implementation)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha256_engine_as_ref(implementation: oxi_implementation_t) -> *const oxi_sha_engine_t
-{
-  oxi_sha_engine_t::as_ref(Variant::Sha256, implementation)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha384_engine_new(implementation: oxi_implementation_t) -> oxi_sha_engine_t
-{
-  oxi_sha_engine_t::new(Variant::Sha384, implementation)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha384_engine_as_ref(implementation: oxi_implementation_t) -> *const oxi_sha_engine_t
-{
-  oxi_sha_engine_t::as_ref(Variant::Sha384, implementation)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_engine_new(implementation: oxi_implementation_t) -> oxi_sha_engine_t
-{
-  oxi_sha_engine_t::new(Variant::Sha512, implementation)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_engine_as_ref(implementation: oxi_implementation_t) -> *const oxi_sha_engine_t
-{
-  oxi_sha_engine_t::as_ref(Variant::Sha512, implementation)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_224_engine_new(implementation: oxi_implementation_t) -> oxi_sha_engine_t
-{
-  oxi_sha_engine_t::new(Variant::Sha512_224, implementation)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_224_engine_as_ref(implementation: oxi_implementation_t) -> *const oxi_sha_engine_t
-{
-  oxi_sha_engine_t::as_ref(Variant::Sha512_224, implementation)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_256_engine_new(implementation: oxi_implementation_t) -> oxi_sha_engine_t
-{
-  oxi_sha_engine_t::new(Variant::Sha512_256, implementation)
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_256_engine_as_ref(implementation: oxi_implementation_t) -> *const oxi_sha_engine_t
-{
-  oxi_sha_engine_t::as_ref(Variant::Sha512_256, implementation)
-}
+pub type oxi_sha_engine1_t = sha::Engine1;
+#[allow(non_camel_case_types)]
+pub type oxi_sha_engine256_t = sha::Engine256;
+#[allow(non_camel_case_types)]
+pub type oxi_sha_engine512_t = sha::Engine512;
 
 // SHA contexts.
 
 #[allow(non_camel_case_types)]
-pub type oxi_sha1_t = Sha1;
-
+type oxi_sha1_t = Sha1;
 #[allow(non_camel_case_types)]
-pub type oxi_sha224_t = Sha224;
-
+type oxi_sha224_t = Sha224;
 #[allow(non_camel_case_types)]
-pub type oxi_sha256_t = Sha256;
-
+type oxi_sha256_t = Sha256;
 #[allow(non_camel_case_types)]
-pub type oxi_sha384_t = Sha384;
-
+type oxi_sha384_t = Sha384;
 #[allow(non_camel_case_types)]
-pub type oxi_sha512_t = Sha512;
-
+type oxi_sha512_t = Sha512;
 #[allow(non_camel_case_types)]
-pub type oxi_sha512_224_t = Sha512_224;
-
+type oxi_sha512_224_t = Sha512_224;
 #[allow(non_camel_case_types)]
-pub type oxi_sha512_256_t = Sha512_256;
+type oxi_sha512_256_t = Sha512_256;
 
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha1_reset(ctx: *mut oxi_sha1_t)
-{
-  let ctx = &mut *ctx;
-  ctx.reset();
+// SHA functions.
+
+macro_rules! impl_sha {
+  (
+    type $sha:ident;
+    fn reset = $reset:ident;
+    fn update = $update:ident;
+    fn update_impl = $update_impl:ident;
+    fn finish_sliced = $finish_sliced:ident;
+    fn finish_sliced_impl = $finish_sliced_impl:ident;
+    fn finish_into = $finish_into:ident;
+    fn finish_into_impl = $finish_into_impl:ident;
+  ) => {
+    #[no_mangle]
+    pub unsafe extern "C" fn $reset(ctx: *mut $sha)
+    {
+      let ctx: &mut $sha = &mut *ctx;
+      ctx.reset();
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn $update(ctx: *mut $sha, data: *const u8, datalen: usize)
+    {
+      let ctx: &mut $sha = &mut *ctx;
+      ctx.update(slice::from_raw_parts(data, datalen));
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn $update_impl(
+      ctx: *mut $sha,
+      implementation: oxi_implementation_t,
+      data: *const u8,
+      datalen: usize,
+    )
+    {
+      let ctx: &mut $sha = &mut *ctx;
+      ctx.update_impl(implementation, slice::from_raw_parts(data, datalen));
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn $finish_sliced(ctx: *mut $sha) -> *const u8
+    {
+      let ctx: &mut $sha = &mut *ctx;
+      ctx.finish_sliced().as_ptr()
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn $finish_sliced_impl(ctx: *mut $sha, implementation: oxi_implementation_t) -> *const u8
+    {
+      let ctx: &mut $sha = &mut *ctx;
+      ctx.finish_sliced_impl(implementation).as_ptr()
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn $finish_into(ctx: *mut $sha, out: *mut u8, outlen: usize)
+    {
+      let ctx: &mut $sha = &mut *ctx;
+      ctx.finish_into(slice::from_raw_parts_mut(out, outlen));
+    }
+
+    #[no_mangle]
+    pub unsafe extern "C" fn $finish_into_impl(
+      ctx: *mut $sha,
+      implementation: oxi_implementation_t,
+      out: *mut u8,
+      outlen: usize,
+    )
+    {
+      let ctx: &mut $sha = &mut *ctx;
+      ctx.finish_into_impl(implementation, slice::from_raw_parts_mut(out, outlen));
+    }
+  };
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha224_reset(ctx: *mut oxi_sha224_t)
-{
-  let ctx = &mut *ctx;
-  ctx.reset();
+impl_sha! {
+  type oxi_sha1_t;
+  fn reset = oxi_sha1_reset;
+  fn update = oxi_sha1_update;
+  fn update_impl = oxi_sha1_update_impl;
+  fn finish_sliced = oxi_sha1_finish_sliced;
+  fn finish_sliced_impl = oxi_sha1_finish_sliced_impl;
+  fn finish_into = oxi_sha1_finish_into;
+  fn finish_into_impl = oxi_sha1_finish_into_impl;
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha256_reset(ctx: *mut oxi_sha256_t)
-{
-  let ctx = &mut *ctx;
-  ctx.reset();
+impl_sha! {
+  type oxi_sha224_t;
+  fn reset = oxi_sha224_reset;
+  fn update = oxi_sha224_update;
+  fn update_impl = oxi_sha224_update_impl;
+  fn finish_sliced = oxi_sha224_finish_sliced;
+  fn finish_sliced_impl = oxi_sha224_finish_sliced_impl;
+  fn finish_into = oxi_sha224_finish_into;
+  fn finish_into_impl = oxi_sha224_finish_into_impl;
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha384_reset(ctx: *mut oxi_sha384_t)
-{
-  let ctx = &mut *ctx;
-  ctx.reset();
+impl_sha! {
+  type oxi_sha256_t;
+  fn reset = oxi_sha256_reset;
+  fn update = oxi_sha256_update;
+  fn update_impl = oxi_sha256_update_impl;
+  fn finish_sliced = oxi_sha256_finish_sliced;
+  fn finish_sliced_impl = oxi_sha256_finish_sliced_impl;
+  fn finish_into = oxi_sha256_finish_into;
+  fn finish_into_impl = oxi_sha256_finish_into_impl;
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_reset(ctx: *mut oxi_sha512_t)
-{
-  let ctx = &mut *ctx;
-  ctx.reset();
+impl_sha! {
+  type oxi_sha384_t;
+  fn reset = oxi_sha384_reset;
+  fn update = oxi_sha384_update;
+  fn update_impl = oxi_sha384_update_impl;
+  fn finish_sliced = oxi_sha384_finish_sliced;
+  fn finish_sliced_impl = oxi_sha384_finish_sliced_impl;
+  fn finish_into = oxi_sha384_finish_into;
+  fn finish_into_impl = oxi_sha384_finish_into_impl;
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_224_reset(ctx: *mut oxi_sha512_224_t)
-{
-  let ctx = &mut *ctx;
-  ctx.reset();
+impl_sha! {
+  type oxi_sha512_t;
+  fn reset = oxi_sha512_reset;
+  fn update = oxi_sha512_update;
+  fn update_impl = oxi_sha512_update_impl;
+  fn finish_sliced = oxi_sha512_finish_sliced;
+  fn finish_sliced_impl = oxi_sha512_finish_sliced_impl;
+  fn finish_into = oxi_sha512_finish_into;
+  fn finish_into_impl = oxi_sha512_finish_into_impl;
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_256_reset(ctx: *mut oxi_sha512_256_t)
-{
-  let ctx = &mut *ctx;
-  ctx.reset();
+impl_sha! {
+  type oxi_sha512_224_t;
+  fn reset = oxi_sha512_224_reset;
+  fn update = oxi_sha512_224_update;
+  fn update_impl = oxi_sha512_224_update_impl;
+  fn finish_sliced = oxi_sha512_224_finish_sliced;
+  fn finish_sliced_impl = oxi_sha512_224_finish_sliced_impl;
+  fn finish_into = oxi_sha512_224_finish_into;
+  fn finish_into_impl = oxi_sha512_224_finish_into_impl;
 }
 
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha1_update(
-  ctx: *mut oxi_sha1_t,
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.update(implementation, slice::from_raw_parts(data, datalen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha224_update(
-  ctx: *mut oxi_sha224_t,
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.update(implementation, slice::from_raw_parts(data, datalen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha256_update(
-  ctx: *mut oxi_sha256_t,
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.update(implementation, slice::from_raw_parts(data, datalen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha384_update(
-  ctx: *mut oxi_sha384_t,
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.update(implementation, slice::from_raw_parts(data, datalen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_update(
-  ctx: *mut oxi_sha512_t,
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.update(implementation, slice::from_raw_parts(data, datalen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_224_update(
-  ctx: *mut oxi_sha512_224_t,
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.update(implementation, slice::from_raw_parts(data, datalen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_256_update(
-  ctx: *mut oxi_sha512_256_t,
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.update(implementation, slice::from_raw_parts(data, datalen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha1_finish(
-  ctx: *mut oxi_sha1_t,
-  implementation: oxi_implementation_t,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.finish_into(implementation, slice::from_raw_parts_mut(out, outlen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha224_finish(
-  ctx: *mut oxi_sha224_t,
-  implementation: oxi_implementation_t,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.finish_into(implementation, slice::from_raw_parts_mut(out, outlen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha256_finish(
-  ctx: *mut oxi_sha256_t,
-  implementation: oxi_implementation_t,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.finish_into(implementation, slice::from_raw_parts_mut(out, outlen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha384_finish(
-  ctx: *mut oxi_sha384_t,
-  implementation: oxi_implementation_t,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.finish_into(implementation, slice::from_raw_parts_mut(out, outlen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_finish(
-  ctx: *mut oxi_sha512_t,
-  implementation: oxi_implementation_t,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.finish_into(implementation, slice::from_raw_parts_mut(out, outlen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_224_finish(
-  ctx: *mut oxi_sha512_224_t,
-  implementation: oxi_implementation_t,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.finish_into(implementation, slice::from_raw_parts_mut(out, outlen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_256_finish(
-  ctx: *mut oxi_sha512_256_t,
-  implementation: oxi_implementation_t,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  let ctx = &mut *ctx;
-  ctx.finish_into(implementation, slice::from_raw_parts_mut(out, outlen));
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha1_finish_sliced(ctx: *mut oxi_sha1_t, implementation: oxi_implementation_t)
--> *const u8
-{
-  let ctx = &mut *ctx;
-  ctx.finish_sliced(implementation).as_ptr()
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha224_finish_sliced(
-  ctx: *mut oxi_sha224_t,
-  implementation: oxi_implementation_t,
-) -> *const u8
-{
-  let ctx = &mut *ctx;
-  ctx.finish_sliced(implementation).as_ptr()
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha256_finish_sliced(
-  ctx: *mut oxi_sha256_t,
-  implementation: oxi_implementation_t,
-) -> *const u8
-{
-  let ctx = &mut *ctx;
-  ctx.finish_sliced(implementation).as_ptr()
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha384_finish_sliced(
-  ctx: *mut oxi_sha384_t,
-  implementation: oxi_implementation_t,
-) -> *const u8
-{
-  let ctx = &mut *ctx;
-  ctx.finish_sliced(implementation).as_ptr()
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_finish_sliced(
-  ctx: *mut oxi_sha512_t,
-  implementation: oxi_implementation_t,
-) -> *const u8
-{
-  let ctx = &mut *ctx;
-  ctx.finish_sliced(implementation).as_ptr()
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_224_finish_sliced(
-  ctx: *mut oxi_sha512_224_t,
-  implementation: oxi_implementation_t,
-) -> *const u8
-{
-  let ctx = &mut *ctx;
-  ctx.finish_sliced(implementation).as_ptr()
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_256_finish_sliced(
-  ctx: *mut oxi_sha512_256_t,
-  implementation: oxi_implementation_t,
-) -> *const u8
-{
-  let ctx = &mut *ctx;
-  ctx.finish_sliced(implementation).as_ptr()
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha1_oneshot(
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  Sha1::oneshot_into(
-    implementation,
-    slice::from_raw_parts(data, datalen),
-    slice::from_raw_parts_mut(out, outlen),
-  );
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha224_oneshot(
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  Sha224::oneshot_into(
-    implementation,
-    slice::from_raw_parts(data, datalen),
-    slice::from_raw_parts_mut(out, outlen),
-  );
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha256_oneshot(
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  Sha256::oneshot_into(
-    implementation,
-    slice::from_raw_parts(data, datalen),
-    slice::from_raw_parts_mut(out, outlen),
-  );
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha384_oneshot(
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  Sha384::oneshot_into(
-    implementation,
-    slice::from_raw_parts(data, datalen),
-    slice::from_raw_parts_mut(out, outlen),
-  );
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_oneshot(
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  Sha512::oneshot_into(
-    implementation,
-    slice::from_raw_parts(data, datalen),
-    slice::from_raw_parts_mut(out, outlen),
-  );
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_224_oneshot(
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  Sha512_224::oneshot_into(
-    implementation,
-    slice::from_raw_parts(data, datalen),
-    slice::from_raw_parts_mut(out, outlen),
-  );
-}
-
-#[no_mangle]
-pub unsafe extern "C" fn oxi_sha512_256_oneshot(
-  implementation: oxi_implementation_t,
-  data: *const u8,
-  datalen: usize,
-  out: *mut u8,
-  outlen: usize,
-)
-{
-  Sha512_256::oneshot_into(
-    implementation,
-    slice::from_raw_parts(data, datalen),
-    slice::from_raw_parts_mut(out, outlen),
-  );
+impl_sha! {
+  type oxi_sha512_256_t;
+  fn reset = oxi_sha512_256_reset;
+  fn update = oxi_sha512_256_update;
+  fn update_impl = oxi_sha512_256_update_impl;
+  fn finish_sliced = oxi_sha512_256_finish_sliced;
+  fn finish_sliced_impl = oxi_sha512_256_finish_sliced_impl;
+  fn finish_into = oxi_sha512_256_finish_into;
+  fn finish_into_impl = oxi_sha512_256_finish_into_impl;
 }
