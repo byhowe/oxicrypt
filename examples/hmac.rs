f14e6eb335af14ce8bf8464abc67dd92cab64e57
B. Howe <37745048+byhowe@users.noreply.github.com>
2021-10-14 18:29:51 +0200

    Comment out hmac example.

--- examples/hmac.rs
+++ examples/hmac.rs
@@ -1,40 +1,40 @@
 extern crate std;
 
-use std::io::stdin;
-use std::io::stdout;
-use std::io::Write;
-
-use oxicrypt::hmac;
-use oxicrypt::Control;
-use oxicrypt::Implementation;
+// use std::io::stdin;
+// use std::io::stdout;
+// use std::io::Write;
+//
+// use oxicrypt::hmac;
+// use oxicrypt::Control;
+// use oxicrypt::Implementation;
 
 fn main() -> std::io::Result<()>
 {
-  Control::set_global_implementation(Implementation::fastest_rt());
+  // Control::set_global_implementation(Implementation::fastest_rt());
 
-  let mut sout = stdout();
-  let sin = stdin();
+  // let mut sout = stdout();
+  // let sin = stdin();
 
-  let mut buffer = String::new();
+  // let mut buffer = String::new();
 
-  sout.write_all(b"Enter key: ")?;
-  sout.flush()?;
-  sin.read_line(&mut buffer)?;
+  // sout.write_all(b"Enter key: ")?;
+  // sout.flush()?;
+  // sin.read_line(&mut buffer)?;
 
-  let mut h = hmac::HmacSha256::with_key(buffer[0 .. buffer.len() - 1].as_bytes());
+  // let mut h = hmac::HmacSha256::with_key(buffer[0 .. buffer.len() - 1].as_bytes());
 
-  buffer.clear();
+  // buffer.clear();
 
-  sout.write_all(b"Enter data: ")?;
-  sout.flush()?;
-  sin.read_line(&mut buffer)?;
+  // sout.write_all(b"Enter data: ")?;
+  // sout.flush()?;
+  // sin.read_line(&mut buffer)?;
 
-  h.update(buffer[0 .. buffer.len() - 1].as_bytes());
-  let digest = h.finish_sliced();
-  let encoded = hex::encode(digest);
-  sout.write_all(encoded.as_bytes())?;
-  sout.write_all(b"\n")?;
-  sout.flush()?;
+  // h.update(buffer[0 .. buffer.len() - 1].as_bytes());
+  // let digest = h.finish_sliced();
+  // let encoded = hex::encode(digest);
+  // sout.write_all(encoded.as_bytes())?;
+  // sout.write_all(b"\n")?;
+  // sout.flush()?;
 
   Ok(())
 }
