63e941e0bd21e174bc92c92cebe648abe3fa3ccb
B. Howe <37745048+byhowe@users.noreply.github.com>
2022-11-19 13:41:53 +0300

    Add `AesVectors::read_from_bytes` function

--- oxicrypt-test-vectors/src/aes.rs
+++ oxicrypt-test-vectors/src/aes.rs
@@ -1,3 +1,5 @@
+use crate::BytesReader;
+
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum Aes {
     Aes128,
@@ -32,6 +34,51 @@ where
     pub key: [u8; V.key_length()],
     pub expanded_key: [u8; V.expanded_key_length()],
     pub inversed_key: [u8; V.expanded_key_length()],
-    pub plaintext: [[u8; 16]; 8],
-    pub ciphertext: [[u8; 16]; 8],
+    pub plaintext: [u8; 16 * 8],
+    pub ciphertext: [u8; 16 * 8],
+}
+
+impl<const V: Aes> Default for AesVectors<V>
+where
+    [(); V.key_length()]:,
+    [(); V.expanded_key_length()]:,
+{
+    fn default() -> Self {
+        Self {
+            key: [0; V.key_length()],
+            expanded_key: [0; V.expanded_key_length()],
+            inversed_key: [0; V.expanded_key_length()],
+            plaintext: [0; 16 * 8],
+            ciphertext: [0; 16 * 8],
+        }
+    }
+}
+
+impl<const V: Aes> AesVectors<V>
+where
+    [(); V.key_length()]:,
+    [(); V.expanded_key_length()]:,
+{
+    pub fn read_from_bytes(raw: &[u8]) -> AesVectors<V> {
+        let mut vectors = AesVectors::default();
+        let mut buffer = BytesReader::new(raw);
+
+        vectors
+            .key
+            .clone_from_slice(buffer.next_n::<{ V.key_length() }>());
+        vectors
+            .expanded_key
+            .clone_from_slice(buffer.next_n::<{ V.expanded_key_length() }>());
+        vectors
+            .inversed_key
+            .clone_from_slice(buffer.next_n::<{ V.expanded_key_length() }>());
+        vectors
+            .plaintext
+            .clone_from_slice(buffer.next_n::<{ 16 * 8 }>());
+        vectors
+            .ciphertext
+            .clone_from_slice(buffer.next_n::<{ 16 * 8 }>());
+
+        vectors
+    }
 }
--- oxicrypt-test-vectors/src/lib.rs
+++ oxicrypt-test-vectors/src/lib.rs
@@ -6,3 +6,20 @@ mod aes;
 
 pub use aes::Aes;
 pub use aes::AesVectors;
+
+pub(crate) struct BytesReader<'a> {
+    buffer: &'a [u8],
+    index: usize,
+}
+
+impl<'a> BytesReader<'a> {
+    pub fn new(buffer: &'a [u8]) -> Self {
+        Self { buffer, index: 0 }
+    }
+
+    pub fn next_n<const N: usize>(&mut self) -> &[u8] {
+        let slice = &self.buffer[self.index..self.index + N];
+        self.index += N;
+        slice
+    }
+}
