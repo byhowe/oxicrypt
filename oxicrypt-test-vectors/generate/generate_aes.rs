420bfc6185248e5026622d45c6649b4990ba5650
B. Howe <37745048+byhowe@users.noreply.github.com>
2022-11-20 17:14:23 +0300

    Rustfmt `generate/generate_aes.rs`

--- oxicrypt-test-vectors/generate/generate_aes.rs
+++ oxicrypt-test-vectors/generate/generate_aes.rs
@@ -1,9 +1,9 @@
 #![feature(box_syntax)]
 
-use std::{path::PathBuf, fs::File};
 use std::io::Write;
+use std::{fs::File, path::PathBuf};
 
-use oxicrypt_test_vectors::{AesVectors, Aes};
+use oxicrypt_test_vectors::{Aes, AesVectors};
 
 const VECTORS_LEN: u32 = 128;
 
@@ -15,7 +15,7 @@ fn main() {
     let mut aes128 = File::create(outpath.join("aes128.bin")).unwrap();
     aes128.write_all(&VECTORS_LEN.to_be_bytes()).unwrap();
     for _ in 0..VECTORS_LEN {
-        let mut vectors = box AesVectors::<{Aes::Aes128}>::default();
+        let mut vectors = box AesVectors::<{ Aes::Aes128 }>::default();
         vectors.set_random();
         let n = vectors.write_to_bytes(&mut buffer);
         aes128.write_all(&buffer[0..n]).unwrap();
@@ -24,7 +24,7 @@ fn main() {
     let mut aes192 = File::create(outpath.join("aes192.bin")).unwrap();
     aes192.write_all(&VECTORS_LEN.to_be_bytes()).unwrap();
     for _ in 0..VECTORS_LEN {
-        let mut vectors = box AesVectors::<{Aes::Aes192}>::default();
+        let mut vectors = box AesVectors::<{ Aes::Aes192 }>::default();
         vectors.set_random();
         let n = vectors.write_to_bytes(&mut buffer);
         aes192.write_all(&buffer[0..n]).unwrap();
@@ -33,7 +33,7 @@ fn main() {
     let mut aes256 = File::create(outpath.join("aes256.bin")).unwrap();
     aes256.write_all(&VECTORS_LEN.to_be_bytes()).unwrap();
     for _ in 0..VECTORS_LEN {
-        let mut vectors = box AesVectors::<{Aes::Aes256}>::default();
+        let mut vectors = box AesVectors::<{ Aes::Aes256 }>::default();
         vectors.set_random();
         let n = vectors.write_to_bytes(&mut buffer);
         aes256.write_all(&buffer[0..n]).unwrap();
