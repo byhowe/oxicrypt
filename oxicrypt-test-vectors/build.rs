f37916175bceed71fac875c42890000f331afa10
B. Howe <37745048+byhowe@users.noreply.github.com>
2022-11-20 12:22:56 +0300

    Move aes vector generation to the Rust library using FFI

---
+++ oxicrypt-test-vectors/build.rs
@@ -0,0 +1,20 @@
+use std::{env, path::PathBuf};
+
+fn main() {
+    cc::Build::new()
+        .file("src/aesni-intel.c")
+        .flag("-maes")
+        .flag("-msse2")
+        .warnings(false)
+        .compile("aesni-intel");
+
+    println!("cargo:rerun-if-changed=src/aesni-intel.h");
+    let bindings = bindgen::builder()
+        .header("src/aesni-intel.h")
+        .generate()
+        .expect("Failed to generate bindings from `aesni-intel.h`");
+    let outpath = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(outpath.join("aesni_intel_bindings.rs"))
+        .expect("Failed to write to `aesni_intel_bindings.rs`");
+}
